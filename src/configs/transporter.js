617c1c1f3f23a6d8f488d746eb0d903f8aede65e
MZakria54321 <muhammadzakria54321@gmail.com>
2025-03-25 00:38:12 +0500

    updated

--- src/configs/transporter.js
+++ src/configs/transporter.js
@@ -1,10 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.secureserver.net",
-  port: 465,
-  // service: "gmail",
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.FROM_EMAIL,
     pass: process.env.PASSWORD,
