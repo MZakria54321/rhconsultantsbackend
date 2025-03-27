51c65b59a43297a7c35c230eeac3d3f50880e87f
MZakria54321 <muhammadzakria54321@gmail.com>
2025-03-28 03:13:18 +0500

    updated

--- src/configs/transporter.js
+++ src/configs/transporter.js
@@ -1,13 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.FROM_EMAIL,
-    pass: process.env.PASSWORD,
-  },
+  // service: "gmail",
+  // auth: {
+  //   user: process.env.FROM_EMAIL,
+  //   pass: process.env.PASSWORD,
+  // },
+  // tls: {
+  //   rejectUnauthorized: false,
+  // },
+
+  host: "smtpout.secureserver.net",
+  secure: true,
+  secureConnection: false,
   tls: {
-    rejectUnauthorized: false,
+    ciphers: "SSLv3",
+  },
+  requireTLS: true,
+  port: 465,
+  debug: true,
+  auth: {
+    user: "info@rheducationconsultants.com",
+    pass: "@Rh#Edc786",
   },
 });
 
